8453b035fed73ccfb8c449ccefd168bf6d4d4318
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-22 11:31:59 -0700

    add jacoco maven plugin

--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import lombok.extern.slf4j.Slf4j;
 
