4d00c37f7941a7c3dda855f8efd7440c9c910723
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-22 10:18:13 -0700

    first commit

---
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/AlterraAcademyFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.alterraacademyfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlterraAcademyFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlterraAcademyFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/AlterraAcademyFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.alterraacademyfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlterraAcademyFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
