187169260e14fa3c654c6cfe02e9a57e4194e938
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-25 08:30:50 -0700

    modify something

--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
@@ -1,24 +1,25 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.controllers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(value = { SpringExtension.class, MockitoExtension.class })
 @Slf4j
 public class HelloWorldControllerTest {
-
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @MockBean
+    public RestTemplate restTemplate;
 
     @Test
     public void testHelloWorld() {
-
+        Assertions.assertNotNull(restTemplate);
     }
 }
