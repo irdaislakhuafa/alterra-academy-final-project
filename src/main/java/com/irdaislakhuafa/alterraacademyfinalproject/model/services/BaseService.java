0e6008ce8c311a6e18d397cd3e3696271e3af196
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-25 15:59:02 -0700

    defina base service

---
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/services/BaseService.java
@@ -0,0 +1,22 @@
+package com.irdaislakhuafa.alterraacademyfinalproject.model.services;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BaseService<A, B> {
+    public Optional<A> save(A entity);
+
+    public Optional<A> update(A entity);
+
+    public Optional<A> findById(String id);
+
+    public List<A> findAll();
+
+    public boolean deleteById(String id);
+
+    public boolean existsById(String id);
+
+    public A mapToEntity(B dto);
+
+    public List<A> mapToEntities(List<B> dtos);
+}
