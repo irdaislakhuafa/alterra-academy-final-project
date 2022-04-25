f79a9942247430da66445250321bb984259a1c3c
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-25 15:49:01 -0700

    add simple validation

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/entities/Author.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/entities/Author.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,14 +28,18 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Author extends BaseEntity {
-
     @Column(nullable = false, length = 100)
+    @NotNull(message = "first name cannot be null")
+    @NotEmpty(message = "first name cannot be empty")
     private String firstName;
 
     @Column(nullable = true, length = 100)
     private String lastName;
 
     @Column(unique = true, length = 100, nullable = false)
+    @NotNull(message = "email cannot be null")
+    @NotEmpty(message = "email cannot be empty")
+    @Email(message = "email is not valid")
     private String email;
 
     @OneToMany
