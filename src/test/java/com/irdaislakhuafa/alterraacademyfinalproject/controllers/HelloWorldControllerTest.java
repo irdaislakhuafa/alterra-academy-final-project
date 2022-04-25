cb3e87c91b254396b194f4618ba87285b6aa743d
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-04-25 19:59:38 +0700

    add erd images

--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
@@ -1,8 +1,9 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.controllers;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,9 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 @ExtendWith(SpringExtension.class)
 @Slf4j
 public class HelloWorldControllerTest {
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     public void testHelloWorld() {
-        log.info("Start test hello world controller");
-        Assertions.assertEquals("Hello World", "Hello World");
+
     }
 }
