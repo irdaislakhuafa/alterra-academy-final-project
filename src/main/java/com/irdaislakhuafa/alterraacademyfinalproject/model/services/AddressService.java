af921f44ee0374028546690433eb23659be8a59e
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-26 02:14:45 -0700

    implement base service

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/services/AddressService.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/services/AddressService.java
@@ -2,6 +2,7 @@ package com.irdaislakhuafa.alterraacademyfinalproject.model.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.transaction.Transactional;
 
@@ -12,7 +13,9 @@ import com.irdaislakhuafa.alterraacademyfinalproject.model.repositories.AddressR
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -21,42 +24,71 @@ public class AddressService implements BaseService<Address, AddressDto> {
 
     @Override
     public Optional<Address> save(Address entity) {
-        return null;
+        log.info("Saving new address");
+        var savedAddress = Optional.of(addressRepository.save(entity));
+        log.info("Success saveing new address");
+        return savedAddress;
     }
 
     @Override
     public Optional<Address> update(Address entity) {
-        return null;
+        log.info("Updating address id: " + entity.getId());
+        var updatedAddress = Optional.of(addressRepository.save(entity));
+        log.info("Success updating address id: " + entity.getId());
+        return updatedAddress;
     }
 
     @Override
     public Optional<Address> findById(String id) {
-        return null;
+        log.info("Preparing get address with id: " + id);
+        var address = addressRepository.findById(id);
+
+        if (!address.isPresent()) {
+            log.info("Address with id: " + id + " not found");
+            return Optional.empty();
+        }
+        log.info("Found address with id: " + id);
+        return address;
     }
 
     @Override
     public List<Address> findAll() {
-        return null;
+        log.info("Find all address");
+        var allAddress = addressRepository.findAll();
+        log.info("Success get all address");
+        return allAddress;
     }
 
     @Override
     public boolean deleteById(String id) {
-        return false;
+        if (!this.existsById(id)) {
+            log.info("Address with id: " + id + " not found");
+            return false;
+        }
+        log.info("Success delete address id: " + id);
+        addressRepository.deleteById(id);
+        return true;
     }
 
     @Override
     public boolean existsById(String id) {
-        return false;
+        return addressRepository.existsById(id);
     }
 
     @Override
     public Address mapToEntity(AddressDto dto) {
-        return null;
+        log.info("Mapping address dto to entity");
+        var address = Address.builder()
+                .city(dto.getCity())
+                .country(dto.getCountry())
+                .build();
+        log.info("Success address dto to entity");
+        return address;
     }
 
     @Override
     public List<Address> mapToEntities(List<AddressDto> dtos) {
-        return null;
+        return dtos.stream().map((dto) -> this.mapToEntity(dto)).collect(Collectors.toList());
     }
 
 }
