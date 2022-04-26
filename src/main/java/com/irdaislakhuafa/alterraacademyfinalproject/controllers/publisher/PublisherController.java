bb4234d7c215e79bbbe1b67463b9fedcc139d384
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-26 13:34:56 -0700

    add endpoint to save new publisher

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/publisher/PublisherController.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/publisher/PublisherController.java
@@ -1,17 +1,64 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.controllers.publisher;
 
+import javax.validation.Valid;
+
+import com.irdaislakhuafa.alterraacademyfinalproject.model.dtos.PublisherDto;
+import com.irdaislakhuafa.alterraacademyfinalproject.model.services.AddressService;
+import com.irdaislakhuafa.alterraacademyfinalproject.model.services.PublisherService;
+import com.irdaislakhuafa.alterraacademyfinalproject.utils.ApiMessage;
+import com.irdaislakhuafa.alterraacademyfinalproject.utils.ApiResponse;
+import com.irdaislakhuafa.alterraacademyfinalproject.utils.ApiValidation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
-@RequestMapping(value = { "/publisher" })
+@RequiredArgsConstructor
+@RequestMapping(value = { "/publishers" })
 public class PublisherController {
-    @GetMapping
-    public ResponseEntity<?> save(@RequestParam String param) {
-        return null;
-    }
+    private final PublisherService publisherService;
+    private final ApiValidation apiValidation;
+    private final AddressService addressService;
 
+    @PostMapping
+    public ResponseEntity<?> save(@RequestBody @Valid PublisherDto publisherDto, Errors errors) {
+        ApiResponse<?> apiResponse = null;
+
+        if (errors.hasErrors()) {
+            apiResponse = ApiResponse.builder()
+                    .message(ApiMessage.FAILED)
+                    .error(this.apiValidation.getErrorMessages(errors))
+                    .build();
+            return ResponseEntity.badRequest().body(apiResponse);
+        }
+
+        try {
+            var publisher = publisherService.mapToEntity(publisherDto);
+            this.addressService.save(publisher.getAddress());
+            var savedPublisher = publisherService.save(publisher);
+            apiResponse = ApiResponse.builder()
+                    .message(ApiMessage.SUCCESS)
+                    .error(null)
+                    .data(savedPublisher)
+                    .build();
+            return ResponseEntity.ok().body(apiResponse);
+
+        } catch (Exception e) {
+            log.error("Error: " + e.getMessage());
+            apiResponse = ApiResponse.builder()
+                    .message(ApiMessage.ERROR)
+                    .error(e.getMessage())
+                    .data(null)
+                    .build();
+            return ResponseEntity.internalServerError().body(apiResponse);
+        }
+    }
 }
