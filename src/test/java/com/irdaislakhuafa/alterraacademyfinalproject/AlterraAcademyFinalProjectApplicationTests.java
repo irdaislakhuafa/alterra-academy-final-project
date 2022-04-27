936a1cd8a69798a3645839a4c214813cd3ec8984
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-27 06:29:49 -0700

    remove unused imports

--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/AlterraAcademyFinalProjectApplicationTests.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/AlterraAcademyFinalProjectApplicationTests.java
@@ -1,6 +1,5 @@
 package com.irdaislakhuafa.alterraacademyfinalproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/HelloWorldControllerTest.java
@@ -1,19 +1,14 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.controllers;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
-import lombok.extern.slf4j.Slf4j;
-
 @ExtendWith(value = { SpringExtension.class, MockitoExtension.class })
-@Slf4j
 public class HelloWorldControllerTest {
     @MockBean
     public RestTemplate restTemplate;
