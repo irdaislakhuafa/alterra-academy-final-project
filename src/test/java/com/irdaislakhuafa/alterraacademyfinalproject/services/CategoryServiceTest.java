954dda09d16dfd3121e04f7ad1fa2ffbe791ef35
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-29 12:09:34 -0700

    add testSaveSuccess

--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/services/CategoryServiceTest.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/services/CategoryServiceTest.java
@@ -1,5 +1,9 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.services;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import java.util.ArrayList;
 
 import com.irdaislakhuafa.alterraacademyfinalproject.SimpleTestNameGenerator;
@@ -7,13 +11,14 @@ import com.irdaislakhuafa.alterraacademyfinalproject.model.entities.Category;
 import com.irdaislakhuafa.alterraacademyfinalproject.model.repositories.CategoryRepository;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @DisplayNameGeneration(value = SimpleTestNameGenerator.class)
 @SpringBootTest
-public class CategoryServiceTest {
+public class CategoryServiceTest implements BaseServiceTest {
 
     @MockBean
     private CategoryRepository categoryRepository;
@@ -27,4 +32,105 @@ public class CategoryServiceTest {
             .description("-")
             .books(new ArrayList<>())
             .build();
+
+    @Test
+    @Override
+    public void testSaveSuccess() {
+        when(this.categoryRepository.save(any())).thenReturn(category);
+        var result = this.categoryService.save(category);
+
+        assertNotNull(result);
+        assertTrue(result.isPresent());
+        assertEquals(result.get(), category);
+    }
+
+    @Test
+    @Override
+    public void testSaveFailed() {
+
+    }
+
+    @Test
+    @Override
+    public void testFindByIdSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testFindByIdFailed() {
+
+    }
+
+    @Test
+    @Override
+    public void testDeleteByIdSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testDeleteByIdFailed() {
+
+    }
+
+    @Test
+    @Override
+    public void testFindAllSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testMapToEntitySuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testMapToEntityFailed() {
+
+    }
+
+    @Test
+    @Override
+    public void testMapToEntitiesSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testMapToEntitiesFailed() {
+
+    }
+
+    @Test
+    @Override
+    public void testSaveAllSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testSaveAllFailed() {
+
+    }
+
+    @Test
+    @Override
+    public void testFindAllByIdSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testUpdateSuccess() {
+
+    }
+
+    @Test
+    @Override
+    public void testUpdateFailed() {
+
+    }
 }
