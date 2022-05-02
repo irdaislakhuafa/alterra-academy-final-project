9867d7d4e76784de1fd81a33cd1fc2ba85090c48
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-02 17:26:20 +0700

    define book service

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/repositories/BookRepository.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/repositories/BookRepository.java
@@ -1,5 +1,7 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.model.repositories;
 
+import java.util.Optional;
+
 import com.irdaislakhuafa.alterraacademyfinalproject.model.entities.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
-
+    public Optional<Book> findByNameEqualsIgnoreCase(String name);
 }
