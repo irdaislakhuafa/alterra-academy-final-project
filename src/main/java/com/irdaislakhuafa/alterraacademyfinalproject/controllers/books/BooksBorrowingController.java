db2f67b620d85a524c4ed0fdda63fb231ceac494
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-07 10:05:42 +0700

    add endpoint to find all

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/books/BooksBorrowingController.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/books/BooksBorrowingController.java
@@ -1,7 +1,11 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.controllers.books;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static com.irdaislakhuafa.alterraacademyfinalproject.utils.ApiResponse.*;
+
+import com.irdaislakhuafa.alterraacademyfinalproject.services.BooksBorrowingService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,5 +15,16 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping(value = { "/books/borrowing" })
 @RequiredArgsConstructor
 public class BooksBorrowingController {
+    private final BooksBorrowingService booksBorrowingService;
 
+    @GetMapping
+    public ResponseEntity<?> findAll() {
+        try {
+            var all = this.booksBorrowingService.findAll();
+            return ResponseEntity.ok(success(all));
+        } catch (Exception e) {
+            log.error("Error: " + e.getMessage());
+            return ResponseEntity.internalServerError().body(error(e.getMessage()));
+        }
+    }
 }
