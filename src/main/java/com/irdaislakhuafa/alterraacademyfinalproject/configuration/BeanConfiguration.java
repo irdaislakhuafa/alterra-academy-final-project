9f3332d57d44ab7af6fa4ee2acf8e35f200d6e49
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-05-07 12:19:28 -0700

    implement UserDetailsService

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/configuration/BeanConfiguration.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/configuration/BeanConfiguration.java
@@ -2,6 +2,7 @@ package com.irdaislakhuafa.alterraacademyfinalproject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,5 +10,10 @@ public class BeanConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/services/UserService.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/services/UserService.java
@@ -1,79 +1,96 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.services;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import javax.transaction.Transactional;
 
 import com.irdaislakhuafa.alterraacademyfinalproject.model.dtos.UserDto;
 import com.irdaislakhuafa.alterraacademyfinalproject.model.entities.User;
+import com.irdaislakhuafa.alterraacademyfinalproject.model.repositories.UserRepository;
 
 import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService implements UserDetailsService, BaseService<User, UserDto> {
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public Optional<User> save(User entity) {
-        // TODO Auto-generated method stub
-        return null;
+        entity.setPassword(this.passwordEncoder.encode(entity.getPassword()));
+        return Optional.of(this.userRepository.save(entity));
     }
 
     @Override
     public Optional<User> update(User entity) {
-        // TODO Auto-generated method stub
-        return null;
+        return Optional.of(this.userRepository.save(entity));
     }
 
     @Override
     public Optional<User> findById(String id) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.userRepository.findById(id);
     }
 
     @Override
     public List<User> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.userRepository.findAll();
     }
 
     @Override
     public boolean deleteById(String id) {
-        // TODO Auto-generated method stub
-        return false;
+        if (!this.existsById(id)) {
+            throw new NoSuchElementException("user with id: " + id + " not found");
+        }
+        return true;
     }
 
     @Override
     public boolean existsById(String id) {
-        // TODO Auto-generated method stub
-        return false;
+        return this.userRepository.existsById(id);
     }
 
     @Override
     public User mapToEntity(UserDto dto) {
-        // TODO Auto-generated method stub
-        return null;
+        return User.builder()
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .roles(new HashSet<>(
+                        this.roleService.findByNames(
+                                dto.getRoles().toArray(
+                                        new String[dto.getRoles().size()]))))
+                .build();
     }
 
     @Override
     public List<User> mapToEntities(List<UserDto> dtos) {
-        // TODO Auto-generated method stub
-        return null;
+        return dtos.stream().map((dto) -> this.mapToEntity(dto)).collect(Collectors.toList());
     }
 
     @Override
     public List<User> findAllById(List<String> ids) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.userRepository.findAllById(ids);
     }
 
     @Override
     public List<User> saveAll(List<User> entities) {
-        // TODO Auto-generated method stub
-        return null;
+        return this.userRepository.saveAll(entities);
     }
 
     @Override
     public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+        return this.userRepository.findByEmailEqualsIgnoreCase(arg0)
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("user with email: " + arg0 + " not found");
+                });
     }
 
 }
