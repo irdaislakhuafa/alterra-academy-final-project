9ce3f3462560b8a3cf58108709507c0eb6a00098
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-07 09:57:38 +0700

    add simple validation

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/dtos/BooksBorrowingDto.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/dtos/BooksBorrowingDto.java
@@ -2,6 +2,10 @@ package com.irdaislakhuafa.alterraacademyfinalproject.model.dtos;
 
 import java.util.List;
 
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.irdaislakhuafa.alterraacademyfinalproject.model.entities.utils.BorrowStatus;
 
 import lombok.*;
@@ -12,8 +16,20 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BooksBorrowingDto {
-    private List<String> booksIds;
-    private List<String> studentsIds;
+    @NotNull(message = "books_ids[] cannot be null")
+    @NotEmpty(message = "books_ids[] cannot be empty")
+    @NotBlank(message = "books_ids[] cannot be blank")
+    private List<@NotNull(message = "books_ids cannot be null") @NotEmpty(message = "books_ids cannot be empty") @NotBlank(message = "books_ids cannot be blank") String> booksIds;
+
+    @NotNull(message = "students_ids cannot be null")
+    @NotEmpty(message = "students_ids cannot be empty")
+    @NotBlank(message = "students_ids cannot be blank")
+    private List<@NotNull(message = "students_ids cannot be null") @NotEmpty(message = "students_ids cannot be empty") @NotBlank(message = "students_ids cannot be blank") String> studentsIds;
+
+    @NotNull(message = "status cannot be null")
+    @NotEmpty(message = "status cannot be empty")
+    @NotBlank(message = "status cannot be blank")
     private BorrowStatus status;
 }
