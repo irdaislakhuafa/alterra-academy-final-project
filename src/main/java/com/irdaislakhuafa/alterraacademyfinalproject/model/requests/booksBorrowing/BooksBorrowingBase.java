8f26cbde01cfbac41e7cb4496b320fbe777a5065
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-05-07 14:18:16 +0700

    define BooksBorrowing request

---
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/requests/booksBorrowing/BooksBorrowingBase.java
@@ -0,0 +1,22 @@
+package com.irdaislakhuafa.alterraacademyfinalproject.model.requests.booksBorrowing;
+
+import javax.validation.constraints.*;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BooksBorrowingBase {
+    @NotNull(message = "books_borrowing_id cannot be null")
+    @NotEmpty(message = "books_borrowing_id cannot be empty")
+    @NotBlank(message = "books_borrowing_id cannot be blank")
+    private String booksBorrowingId;
+}
---
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/model/requests/booksBorrowing/BooksBorrowingStudent.java
@@ -0,0 +1,18 @@
+package com.irdaislakhuafa.alterraacademyfinalproject.model.requests.booksBorrowing;
+
+import javax.validation.constraints.*;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class BooksBorrowingStudent extends BooksBorrowingBase {
+    @NotNull(message = "student_id cannot be null")
+    @NotEmpty(message = "student_id cannot be empty")
+    @NotBlank(message = "student_id cannot be blank")
+    private String studentId;
+}
