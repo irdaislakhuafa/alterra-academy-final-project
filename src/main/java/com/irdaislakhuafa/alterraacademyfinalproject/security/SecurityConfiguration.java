af34345ea15b7b00f198110f35df1300113b6e49
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-05-08 08:16:24 -0700

    setup AuthenticationManager

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/security/SecurityConfiguration.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/security/SecurityConfiguration.java
@@ -1,11 +1,27 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.security;
 
+import com.irdaislakhuafa.alterraacademyfinalproject.services.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserService userService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // start
@@ -31,4 +47,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         // end
         ;
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(new DaoAuthenticationProvider() {
+            {
+                setPasswordEncoder(passwordEncoder);
+                setUserDetailsService(userService);
+            }
+        });
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
 }
