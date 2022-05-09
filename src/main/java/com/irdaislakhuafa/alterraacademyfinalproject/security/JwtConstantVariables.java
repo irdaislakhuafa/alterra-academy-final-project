3e8101de2da2392ba3c62c4385bb1c80d24e772e
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-05-08 18:50:48 -0700

    setup JWT constant variable

---
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/security/JwtConstantVariables.java
@@ -0,0 +1,6 @@
+package com.irdaislakhuafa.alterraacademyfinalproject.security;
+
+public class JwtConstantVariables {
+    public final String AUTHORIZATION = "Authorization ";
+    public final String BEARER = "Bearer ";
+}
---
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/security/JwtPerRequestFilter.java
@@ -0,0 +1,29 @@
+package com.irdaislakhuafa.alterraacademyfinalproject.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class JwtPerRequestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
+            throws ServletException, IOException {
+
+    }
+
+}
