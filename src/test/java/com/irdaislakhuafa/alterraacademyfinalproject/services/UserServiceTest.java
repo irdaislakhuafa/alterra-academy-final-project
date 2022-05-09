75b9cae39de50a75265ae6dfbdc1408746c494e5
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-05-09 00:05:25 -0700

    define Role and user object

--- src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/services/UserServiceTest.java
+++ src/test/java/com/irdaislakhuafa/alterraacademyfinalproject/services/UserServiceTest.java
@@ -1,6 +1,42 @@
 package com.irdaislakhuafa.alterraacademyfinalproject.services;
 
+import java.util.HashSet;
+
+import com.irdaislakhuafa.alterraacademyfinalproject.SimpleTestNameGenerator;
+import com.irdaislakhuafa.alterraacademyfinalproject.model.entities.Role;
+import com.irdaislakhuafa.alterraacademyfinalproject.model.entities.User;
+import com.irdaislakhuafa.alterraacademyfinalproject.model.repositories.UserRepository;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Tag;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+@Tag(value = "userServiceTest")
+@DisplayNameGeneration(value = SimpleTestNameGenerator.class)
 public class UserServiceTest implements BaseServiceTest {
+    @MockBean
+    private UserRepository userRepository;
+
+    private final Role role = Role.builder()
+            .id("roleId")
+            .name("ROLE_ADMIN")
+            .description("-")
+            .build();
+
+    private final User user = User.builder()
+            .id("userId")
+            .name("irda islakhu afa")
+            .email("irdhaislakhuafa@gmail.com")
+            .password("anaardani")
+            .isEnable(true)
+            .roles(new HashSet<>() {
+                {
+                    add(role);
+                }
+            })
+            .build();
 
     @Override
     public void testSaveSuccess() {
