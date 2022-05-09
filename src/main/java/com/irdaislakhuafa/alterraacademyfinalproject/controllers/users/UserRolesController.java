cdced9b6d1305e84eaeaed7f9c51b8e99e1d2045
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-05-09 11:19:44 -0700

    create endpoint to add roles to user

--- src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/users/UserRolesController.java
+++ src/main/java/com/irdaislakhuafa/alterraacademyfinalproject/controllers/users/UserRolesController.java
@@ -2,11 +2,16 @@ package com.irdaislakhuafa.alterraacademyfinalproject.controllers.users;
 
 import javax.validation.Valid;
 
+import com.irdaislakhuafa.alterraacademyfinalproject.model.requests.ApiChangeRequests;
 import com.irdaislakhuafa.alterraacademyfinalproject.model.requests.ApiTargetIdRequest;
+import com.irdaislakhuafa.alterraacademyfinalproject.services.RoleService;
 import com.irdaislakhuafa.alterraacademyfinalproject.services.UserService;
 import com.irdaislakhuafa.alterraacademyfinalproject.utils.ApiValidation;
 
 import static com.irdaislakhuafa.alterraacademyfinalproject.utils.ApiResponse.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
@@ -22,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 public class UserRolesController {
     private final UserService userService;
     private final ApiValidation apiValidation;
+    private final RoleService roleService;
 
     @GetMapping
     public ResponseEntity<?> getRoles(@RequestBody @Valid ApiTargetIdRequest request, Errors errors) {
@@ -38,6 +44,33 @@ public class UserRolesController {
             }
 
             return ResponseEntity.ok(success(user.get().getRoles()));
+        } catch (Exception e) {
+            log.error("Error: " + e.getMessage());
+            return ResponseEntity.internalServerError().body(error(e.getMessage()));
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addRoles(@RequestBody @Valid ApiChangeRequests<List<String>> requests, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(validationFailed(apiValidation.getErrorMessages(errors)));
+        }
+
+        try {
+            var roles = this.roleService.findByNames(requests.getData().toArray(new String[requests.getData().size()]));
+            var user = this.userService.findById(requests.getTargetId());
+
+            if (!user.isPresent()) {
+                var message = "user with id: " + requests.getTargetId() + " not found";
+                log.warn(message);
+                return ResponseEntity.badRequest().body(failed(message));
+            }
+
+            user.get().getRoles().addAll(roles);
+            user = this.userService.update(user.get());
+            return ResponseEntity.ok(success(user));
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.badRequest().body(failed(e.getMessage()));
         } catch (Exception e) {
             log.error("Error: " + e.getMessage());
             return ResponseEntity.internalServerError().body(error(e.getMessage()));
